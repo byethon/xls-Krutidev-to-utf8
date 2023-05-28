3007ee030662d98f17e4fcfe5bc084443a0ad2b2
Gurmehar Singh <gurmeharksp@gmail.com>
2023-05-28 14:43:40 +0530

    visual fixes

--- kruti-to-utf-xls.py
+++ kruti-to-utf-xls.py
@@ -25,6 +25,16 @@ def isfloat(num):
     except ValueError:
         return False
 
+def sane_write(inputval):
+    if(isfloat(inputval)):
+        if(float(inputval)-int(float(inputval))==0):
+            write_conv=kd2u8(str(int(inputval)))
+        else:
+            write_conv=kd2u8(str(inputval))
+    else:
+        write_conv=kd2u8(inputval)
+    return write_conv
+
 def processxls(inputfile,outputfile):
     wb2=xlwt.Workbook()
     wb=xlrd.open_workbook(inputfile)
@@ -42,24 +52,20 @@ def processxls(inputfile,outputfile):
 
         for i in range(rmax):
             pvalue=round(i/(rmax-1)*100,2)
+            overall_val=round(100.00*(s-1)/smax+pvalue/smax,2)
             ins=''
             for k in range(int(pvalue/5)):
                 ins=ins+'#'
                 if(k==19):
                     ins=ins+'\033[1D#]'
-            print(f'\033[A [{bcolors.OKBLUE}{pvalue:.2f}{bcolors.ENDC}%{ins}{bcolors.OKBLUE}#{bcolors.ENDC}  ')
+            print(f'\033[A [{bcolors.OKGREEN}{overall_val:.2f}%{bcolors.ENDC}][{bcolors.OKBLUE}{pvalue:.2f}{bcolors.ENDC}%][{ins}{bcolors.OKBLUE}#{bcolors.ENDC}  ')
             for j in range(cmax):
-                if(isfloat(sh.cell_value(i,j))):
-                    if(float(sh.cell_value(i,j))-int(float(sh.cell_value(i,j)))==0):
-                        newsheet.write(i,j,kd2u8(str(int(sh.cell_value(i,j)))))
-                    else:
-                        newsheet.write(i,j,kd2u8(str(sh.cell_value(i,j))))
-                else:
-                    newsheet.write(i,j,kd2u8(sh.cell_value(i,j)))
+                write_conv=sane_write(sh.cell_value(i,j))
+                newsheet.write(i,j,write_conv)
         print(f'Writing:{bcolors.OKBLUE}{outputfile}{bcolors.ENDC}\n')
         wb2.save(outputfile)
 
 try:
     processxls(argv[1],argv[2])
-except:
-    print("ERROR")
+except Exception as e:
+    print(e)
